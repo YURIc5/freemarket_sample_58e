73a3a9f39f2b9920b66492e4793cc77676b966f1
YURIc5 <arewane.c5@gmail.com>
2019-09-13 18:44:23 +0900

    Mostly-completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :set_user, onry: [:new]
+
   def index
   end
 
   def new
+    @item = Item.new
+    @categories = Category.all.name
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
