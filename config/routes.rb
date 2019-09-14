31a172fdebe1f3a900e7909853383caaa2716fff
tk1128kami <tk1128kami@icloud.com>
2019-09-14 16:57:22 +0900

    profile-create

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   resources :users, only: [:show] do
+    member do
+      get'profile'
+    end
     resources :cards, only: [:index, :new, :create, :delete]
   end
 end
