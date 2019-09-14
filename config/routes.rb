c2ed53def6f0f0a883489bd18ca148f534c5c073
kamotetu <lewd_magnifico.m@icloud.com>
2019-09-14 12:59:14 +0900

    cleate_mypage_conplate

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :users, only: [:index]
+  resources :users, only: [:show] do
+    resources :cards, only: [:index, :new, :create, :delete]
+  end
 end
