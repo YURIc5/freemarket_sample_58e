4ec7dbdd1ed09599e083a4bb0642359388652ec9
daburaaa <ryotugboat@gmail.com>
2019-09-14 12:01:25 +0900

    add phone routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
+
+  get 'users/phone' => 'users#phone' 
   resources :users, only: [:show] do
     resources :cards, only: [:index, :new, :create, :delete]
   end
+
+  # resources :user do
+  #   get 'new_phone_number' on: :member
+  # end
+
 end
