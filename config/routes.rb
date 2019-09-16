4bf456cbace7c17c28ee107918265b391600ac75
krk2020 <kurogi1989@gmail.com>
2019-09-16 17:19:30 +0900

    商品詳細ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
+  resources :items
   resources :users, only: [:show] do
     resources :cards, only: [:index, :new, :create, :delete]
   end
