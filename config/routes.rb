50a398be528aa032cb09970d9bfd24ee8a6e0774
YURIc5 <arewane.c5@gmail.com>
2019-09-19 23:45:47 +0900

    Session-description-to-phone

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      get 'done' # 登録完了後のページ
+    end
+  end
   
   get 'users/phone' => 'users#phone' 
   get 'users/signup' => 'users#signup' 
