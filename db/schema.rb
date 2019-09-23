a7b8acc33d4c7143ec9c309e7981f3e3ea4b9c4c
YURIc5 <arewane.c5@gmail.com>
2019-09-23 12:48:33 +0900

    usersテーブルにproviderカラムとuidカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_060017) do
+ActiveRecord::Schema.define(version: 2019_09_23_034448) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode"
@@ -112,6 +112,8 @@ ActiveRecord::Schema.define(version: 2019_09_20_060017) do
     t.datetime "updated_at", null: false
     t.bigint "address_id"
     t.bigint "creditcard_id"
+    t.string "provider"
+    t.string "uid"
     t.index ["address_id"], name: "index_users_on_address_id"
     t.index ["creditcard_id"], name: "index_users_on_creditcard_id"
     t.index ["email"], name: "index_users_on_email", unique: true
