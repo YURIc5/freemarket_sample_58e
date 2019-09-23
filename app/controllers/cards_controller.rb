3314e6f94fd55d913f519ef84b728933fb5427cc
daburaaa <ryotugboat@gmail.com>
2019-09-23 16:32:52 +0900

    delete binding.pry

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,7 +9,6 @@ class CardsController < ApplicationController
 
   def create
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-    binding.pry
     customer = Payjp::Customer.create(card: params[:payjpToken])
     @creditcard = Creditcard.new(user_id: current_user.id, customer_id: customer.id, card_token: params[:payjpToken])
     redirect_to root_path unless @creditcard.save
