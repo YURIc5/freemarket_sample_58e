b15ccaee5ede626c2c1b54f8cfa6425f6db23c77
daburaaa <ryotugboat@gmail.com>
2019-09-23 19:08:16 +0900

    delete commentout

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,4 @@
 Recaptcha.configure do |config|
-  # config.site_key = '6Lc91bkUAAAAAA8sr4lMYw_a2qh8ilns1S0XOasJ'
-  # config.secret_key = '6Lc91bkUAAAAAJDMh2RXMhmF-N36WvCSSzbOGYEi'
   config.site_key = Rails.application.credentials.dig(:reCAPTCHA, :site_key)
   config.secret_key = Rails.application.credentials.dig(:reCAPTCHA, :secret_key)
 end
