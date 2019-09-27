923e2f84f5b02a5bf37bfcb15a96729d5f4ad471
YURIc5 <arewane.c5@gmail.com>
2019-09-27 20:58:15 +0900

    prefecture_idカラムの追加とアクティブハッシュの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_024852) do
+
+ActiveRecord::Schema.define(version: 2019_09_27_063405) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode"
@@ -21,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_26_024852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
