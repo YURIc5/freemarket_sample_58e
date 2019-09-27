adcbe2652613954bc82a48ea3da65cd537202c8f
krk2020 <kurogi1989@gmail.com>
2019-09-27 13:24:24 +0900

    top画面から商品詳細のリンク設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_26_024852) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode"
