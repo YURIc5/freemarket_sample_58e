6c3321c315f8a953e17317682c39bd7db2d38116
YURIc5 <arewane.c5@gmail.com>
2019-09-28 14:26:03 +0900

    htmlとscssを記述

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,14 @@
+# crumb :root do
+#   link "Home", root_path
+# end
+
+# ルート
 crumb :root do
-  link "Home", root_path
+  link "メルカリ", root_path
+end
+# マイページ
+crumb :mypage do
+  link "マイページ", user_path
 end
 
 # crumb :projects do
