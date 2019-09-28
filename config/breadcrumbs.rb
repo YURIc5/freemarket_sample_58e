90d6e1ee92f28dd672527e28426f1517295e4a26
YURIc5 <arewane.c5@gmail.com>
2019-09-28 15:48:20 +0900

    パンくずの設定とSCSSの修正

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -10,6 +10,21 @@ end
 crumb :mypage do
   link "マイページ", user_path
 end
+# メルカリ＞マイページ＞プロフィール
+crumb :profile do|profile|
+  link 'プロフィール'
+  parent :mypage
+end
+# メルカリ＞マイページ＞本人情報の登録
+crumb :information do|information|
+  link '本人情報の登録'
+  parent :mypage
+end
+# メルカリ＞マイページ＞ログアウト
+crumb :logout do|logout|
+  link 'ログアウト'
+  parent :mypage
+end
 
 # crumb :projects do
 #   link "Projects", projects_path
