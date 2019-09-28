72076af0c8e3a98e34856df88bfbe85709b7936f
YURIc5 <arewane.c5@gmail.com>
2019-09-28 12:10:05 +0900

    不要になったカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 2019_09_27_063405) do
+ActiveRecord::Schema.define(version: 2019_09_28_030213) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode"
-    t.string "prefecture"
     t.string "city"
     t.integer "block"
     t.string "building"
