d3385f9eee35a6bfda4f051f8c6eb068a631c9f7
YURIc5 <arewane.c5@gmail.com>
2019-10-01 21:27:52 +0900

    出品出品した商品ページにパンくずを追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,18 +6,23 @@ end
 crumb :mypage do
   link "マイページ", user_path
 end
-# メルカリ＞マイページ＞プロフィール
+# メルカリ > マイページ > プロフィール
 crumb :profile do|profile|
   link 'プロフィール'
   parent :mypage
 end
-# メルカリ＞マイページ＞本人情報の登録
+# メルカリ > マイページ > 本人情報の登録
 crumb :information do|information|
   link '本人情報の登録'
   parent :mypage
 end
-# メルカリ＞マイページ＞ログアウト
+# メルカリ > マイページ > ログアウト
 crumb :logout do|logout|
   link 'ログアウト'
+  parent :mypage
+end
+# メルカリ > マイページ > 出品した商品 - 出品中
+crumb :exhibited do|exhibited|
+  link '出品した商品 - 出品中'
   parent :mypage
 end
