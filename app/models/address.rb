0a4d3dc7226a4db2b21efad4123665e1fd2fe22b
YURIc5 <arewane.c5@gmail.com>
2019-10-02 19:45:12 +0900

    住所登録のバリデーションを定義

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,11 @@
 class Address < ApplicationRecord
-    belongs_to :user
-    
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to_active_hash :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :user
+
+  validates :postcode , presence:true ,format:{with:/\A\d{3}[-]\d{4}\z/}
+  validates :prefecture_id  , presence: true ,inclusion: 1..47
+  validates :city           , presence: true , format:{with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :block          , presence: true 
+  validates :building       ,length:{maximum: 25}
 end
