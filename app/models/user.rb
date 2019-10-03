f8774b53059418ec759e24ec5c8d16236695080f
YURIc5 <arewane.c5@gmail.com>
2019-10-03 17:27:53 +0900

    不要なコメントやファイルの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
          kana = /\A[ァ-ヶー－]+\z/
 
   has_many :items
-    has_many :comments
-    has_many :evaluations
-    has_many :purchases
-    has_many :item_likes
-    has_one :address
-    has_one :creditcard
+  has_many :comments
+  has_many :evaluations
+  has_many :purchases
+  has_many :item_likes
+  has_one :address
+  has_one :creditcard
 
   validates :nickname, presence: true
   validates :name, presence: true, format: { with: kanji }
@@ -22,33 +22,24 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
   
   
-    def self.find_for_oauth(auth)
-      #登録済みのUserか確認
-      user = User.where(uid: auth.uid, provider: auth.provider).first
-      #登録済みのUserではなかった場合
-      unless user.present?
-        user = User.new(
-          nickname: auth.info.name,
-          email: auth.info.email,
-          uid: auth.uid,
-          provider: auth.provider
-        )
-      end
-      return user
+  def self.find_for_oauth(auth)
+    #登録済みのUserか確認
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    #登録済みのUserではなかった場合
+    unless user.present?
+      user = User.new(
+        nickname: auth.info.name,
+        email: auth.info.email,
+        uid: auth.uid,
+        provider: auth.provider
+      )
     end
+    return user
+  end
 
   has_many :buyed_items, foreign_key: 'buyer_id', class_name: 'Item'
   has_many :saling_items, -> { where('buyer_id is NULL') }, foreign_key: 'user_id', class_name: 'Item'
   has_many :sold_items, -> { where('buyer_id is not NULL') }, foreign_key: 'user_id', class_name: 'Item'
-
-
-  has_one :address
-  has_one :creditcard
-
-#   "から'に変更したためコメントアウト(挙動に問題なければ削除します)
-#   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
-#   has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "user_id", class_name: "Item"
-#   has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "user_id", class_name: "Item"
 
  
 end
